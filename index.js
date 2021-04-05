7d67f6d5bb21147bb1522dc0935e754f7d896551
Brandon <bmacdonald1986@gmail.com>
2021-04-05 05:45:55 +0000

    created sumItems function

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+
+  if (!Array.isArray(array)) {
+    return array;
+  }
+
+  if (array.length < 1 || array === undefined) {
+    return 0;
+  }
+
+  return parseInt(array.reduce((acc,currVal) => sumItems(acc) + sumItems(currVal)));
+};
 
 module.exports = sumItems;
